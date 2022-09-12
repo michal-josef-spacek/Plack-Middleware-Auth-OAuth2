dbe8df74006aa70a763f89f41bed861575f9c0de
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-12 23:22:11 +0200

    Add doc

--- OAuth2.pm
+++ OAuth2.pm
@@ -213,3 +213,214 @@ sub _create_oauth2_object {
 
 __END__
 
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Plack::Middleware::Auth::OAuth2 - Plack OAuth2 middleware.
+
+=head1 SYNOPSIS
+
+ use Plack::Builder;
+ use Plack::App::Env;
+ use Plack::App::Login;
+
+ my $app = Plack::App::Env->new;
+ builder {
+         enable 'Session';
+         enable 'Auth::OAuth2',
+                'client_id' => '__CLIENT_ID__',
+                'client_secret => '__CLIENT_SECRET__',
+                'app_login' => Plack::App::Login->new,
+                'app_login_url' => sub { $_[0]->login_link($_[1]); },
+                'logout_path' => 'logout',
+                'provider' => 'Google',
+                'redirect_path' => 'code',
+                'scope' => 'email',
+         ;
+         $app;
+ };
+
+=head1 DESCRIPTION
+
+This middleware provides OAuth2 authentication for web application. Uses
+L<LWP::Authen::OAuth2> for implementation.
+
+Prerequisity is use of Plack session management and result is saved to session.
+
+=head1 ACCESSORS
+
+=head2 C<app_login>
+
+Plack application to login.
+
+It's required.
+
+=head2 C<app_login_url>
+
+Callback to set URL from OAuth2 provider to C<app_login()> Plack application.
+
+First argument is C<app_login()> application.
+Second argument is C<$url> variable from OAuth2 provider.
+
+It's required.
+
+=head2 C<client_id>
+
+OAuth2 client id.
+
+It's required.
+
+=head2 C<client_secret>
+
+OAuth2 client secret.
+
+It's required.
+
+=head2 C<logout_path>
+
+Logout path for creating of endpoint, which logout from OAuth2.
+
+Default value is 'logout' (/logout).
+
+=head2 C<lwp_user_agent>
+
+Explicit L<LWP::UserAgent> instance.
+
+Default value is L<LWP::UserAgent> instance inside of L<LWP::Authen::OAuth2>.
+
+=head2 C<redirect_path>
+
+Redirect path for creating of endpoint, which is created for service provider
+use to set authentication.
+
+It's required.
+
+=head2 C<scope>
+
+OAuth2 scopes in string.
+
+Requirement is defined by provider. It's optional.
+
+=head2 C<service_provider>
+
+Service provider.
+
+Possible providers:
+
+=over
+
+=item Dwolla
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::Dwolla>.
+
+=item Google
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::Google>.
+
+=item Line
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::Line>.
+
+=item MediaWiki
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::MediaWiki>.
+
+=item Strava
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::Strava>.
+
+=item Wikimedia
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::Wikimedia>.
+
+=item Withings
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::Withings>.
+
+=item Yahoo
+
+Via module L<LWP::Authen::OAuth2::ServiceProvider::Yahoo>.
+
+=back
+
+=head1 ENDPOINTS
+
+=head2 Logout
+
+Logout endpoint is defined inside of this module by setting C<logout_path>
+(/__LOGOUT_PATH__).
+
+=head2 Redirect
+
+Redirect endpoint is defined inside of this module by setting C<redirect_path>
+(/__REDIRECT_PATH__).
+
+=head1 SESSION VARIABLES
+
+=head2 oauth2.obj
+
+Value is instance of LWP::Authen::OAuth2 used for authentization.
+
+=head2 oauth2.service_provider
+
+Value is authenticated service provider.
+
+=head2 oauth2.token_string
+
+Value is token string.
+
+=head1 ERRORS
+
+ prepare_app():
+         No OAuth2 'client_id' setting.
+         No OAuth2 'client_secret' setting.
+         No login application.
+         No login url call.
+         No redirect path.
+         No service provider.
+
+=head1 DEPENDENCIES
+
+L<English>,
+L<Error::Pure>,
+L<JSON::XS>,
+L<LWP::Authen::OAuth2>,
+L<Plack::Middleware>,
+L<Plack::Response>,
+L<Plack::Session>,
+L<Plack::Util::Accessor>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<LWP::Authen::OAuth2>
+
+Make requests to OAuth2 APIs.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Plack-Middleware-Auth-OAuth2>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2020-2022 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
